565556fdf59369c5e0dcd94a8628e8adde606af4
Bambi <ianvinuya7@gmail.com>
2023-12-04 06:29:29 +0800

    improve_hero_section

--- src/components/Section1.tsx
+++ src/components/Section1.tsx
@@ -24,13 +24,13 @@ function Section1() {
   return (
     <section ref={sectionRef} className="text-center opacity-0">
       <p className="font-courierReg text-colorYellow-0 mb-5">Hi, my name is</p>
-      <h1 className="text-colorWhite-0 font-calibreSemBold text-5xl md:text-7xl font-semibold">
+      <h1 className="text-colorWhite-0 font-calibreSemBold md:text-5xl lg:text-7xl text-3xl font-semibold">
         Vincent Vinuya
       </h1>
-      <h1 className="text-colorgray-0 font-calibreSemBold md:text-7xl mb-5 text-5xl font-semibold">
+      <h1 className="text-colorgray-0 font-calibreSemBold lg:text-7xl mb-5 md:text-5xl text-3xl  font-semibold">
         Aspiring Web Developer
       </h1>
-      <p className="font-calibreReg md:text-xl text-base w-[16rem] md:w-[27rem] mx-auto text-colorWhite-0 mb-5 font-normal">
+      <p className="font-calibreReg md:text-xl text-base w-[16rem] md:w-[27rem] mx-auto text-colorWhite-0 mb-5 mt-8 font-normal">
         I have a passion for turning coffee into code, brewing innovation one
         line at a time. Let's bring your ideas to life!
       </p>
