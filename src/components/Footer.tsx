4ae72d5fdeecff399eee022a6bcd1fee56447b63
Vincent V <vincentvinuya33@gmail.com>
2024-01-26 11:58:18 +0800

    correct_gramatical_error

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="h-12 text-center text-colorWhite-0 font-courierItalic mt-44 text-xs md:text-sm italic">
       Design & Created by: Vincent Vinuya 2023
-      <span className="ml-2">| Inspired from Brittany Chiang v4Portfolio</span>
+      <span className="ml-2">| Inspired by Brittany Chiang v4Portfolio</span>
     </footer>
   );
 }
